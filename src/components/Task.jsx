39e91ca0952cb9f4c34d54aaedb3d676b059e722
Neider Urbano <jjuly8julianur@gmail.com>
2023-02-09 17:51:49 -0500

    resolved activity of the contact list project

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,40 +1,69 @@
-import React,{useState, useEffect} from 'react'
-import { Modal } from './Modal'
+import React, { useState, useEffect } from "react";
+import { Modal } from "./Modal";
 
-const Task = ({dataTask, onClickDelete,onClickRadio, onClickUpdate}) => {
-    const [active,setActive]=useState(dataTask.state)
-    
-    const handleClick=()=>{
-        if(active) onClickRadio(false,dataTask.id)
-        else onClickRadio(true,dataTask.id)
-        setActive(!active)
-    }
+const Task = ({ dataTask, onClickDelete, onClickRadio, onClickUpdate }) => {
+  const [active, setActive] = useState(dataTask.state);
+  const { name, description } = dataTask.task;
+  const { id } = dataTask;
+  const handleClick = () => {
+    if (active) onClickRadio(false, id);
+    else onClickRadio(true, id);
+    setActive(!active);
+  };
 
-    return (
-        <div className="card containerCardTask">
-            <div className="form-check" onClick={()=>handleClick()}>
-                <input className="form-check-input" type="radio" id={dataTask.name+dataTask.id}
-                    checked={active} onChange={(e)=>null}
-                />
-                <label className={`form-check-label ${active && 'active'}`}  htmlFor={dataTask.name+dataTask.id}>
-                    {dataTask.name}
-                </label>
-            </div>
-            <div className="div-buttons">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16"
-                onClick={()=>onClickDelete(dataTask.id)}>
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg>
-                <div data-toggle="modal" data-target={`#exampleModalCenter${dataTask.id}`} className="ml-3"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
-                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                    </svg>
-                </div>
-                <Modal dataTask={dataTask} onClickUpdate={onClickUpdate}/> 
-            </div>
+  return (
+    <div className="card containerCardTask">
+      <div className="form-check" onClick={() => handleClick()}>
+        <input
+          className="form-check-input"
+          type="radio"
+          id={name + id}
+          checked={active}
+          onChange={(e) => null}
+        />
+        <label
+          className={`form-check-label ${active && "active"}`}
+          htmlFor={name + id}
+        >
+          {name}
+        </label>
+      </div>
+      <div className="div-buttons">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-trash"
+          viewBox="0 0 16 16"
+          onClick={() => onClickDelete(id)}
+        >
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+          <path
+            fillRule="evenodd"
+            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+          />
+        </svg>
+        <div
+          data-toggle="modal"
+          data-target={`#exampleModalCenter${id}`}
+          className="ml-3"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-pencil"
+            viewBox="0 0 16 16"
+          >
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+          </svg>
         </div>
-    )
-}
+        <Modal dataTask={dataTask} onClickUpdate={onClickUpdate} />
+      </div>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -1,85 +1,96 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-const useTask=()=>{
-    const [arrayTask,setArrayTask]=useState([])
-    const [taskPending,setTaskPending]=useState(arrayTask.length)
-    const miStorage = window.localStorage;
+const useTask = () => {
+  const [arrayTask, setArrayTask] = useState([]);
+  const [taskPending, setTaskPending] = useState(arrayTask.length);
+  const miStorage = window.localStorage;
 
-    useEffect(()=>{
-        var dataTasks = JSON.parse(miStorage.getItem("dataTasks"))
-        if(dataTasks){
-            setArrayTask(dataTasks)
-            let cantidadPending = 0;
-            dataTasks?.map((task)=>{
-                if(task.state==false){
-                    cantidadPending++;
-                }
-            })
-            setTaskPending(cantidadPending)
+  useEffect(() => {
+    var dataTasks = JSON.parse(miStorage.getItem("dataTasks"));
+    if (dataTasks) {
+      setArrayTask(dataTasks);
+      let cantidadPending = 0;
+      dataTasks?.map((task) => {
+        if (task.state == false) {
+          cantidadPending++;
         }
-    },[taskPending])
+      });
+      setTaskPending(cantidadPending);
+    }
+  }, [taskPending]);
 
-    const handleClickClear=()=>{
-        if(arrayTask.length>0){
-            miStorage.clear();
-            setTaskPending(0)
-            setArrayTask([])
+  const handleClickClear = () => {
+    if (arrayTask.length > 0) {
+      miStorage.clear();
+      setTaskPending(0);
+      setArrayTask([]);
+    }
+  };
+
+  const handleClickDelete = (id) => {
+    var dataTasks = JSON.parse(miStorage.getItem("dataTasks"));
+    let deleteTask = dataTasks.filter((task) => task.id != id);
+    miStorage.setItem("dataTasks", JSON.stringify(deleteTask));
+    setArrayTask(deleteTask);
+    setTaskPending(taskPending - 1);
+  };
+
+  const handleClickRadio = (boolean, idTask) => {
+    var dataTasks = JSON.parse(miStorage.getItem("dataTasks"));
+    let updateTask = dataTasks.map((task) => {
+      if (task.id == idTask) {
+        task.state = boolean;
+      }
+      return task;
+    });
+    miStorage.setItem("dataTasks", JSON.stringify(updateTask));
+    if (boolean) setTaskPending(taskPending - 1);
+    else setTaskPending(taskPending + 1);
+  };
+
+  const handleClickUpdate = (dataUpdateTask) => {
+    miStorage.setItem("dataTasks", JSON.stringify(dataUpdateTask));
+    setTaskPending(null);
+  };
+
+  const handleClickAdd = (dataNewTask) => {
+    if (dataNewTask.name.length > 0) {
+      if (dataNewTask.name.length > 3) {
+        var dataTasks = (dataTasks = JSON.parse(
+          miStorage.getItem("dataTasks")
+        ));
+        if (dataTasks) {
+          dataTasks.push({
+            id: Math.max(...dataTasks.map((task) => task.id)) + 1,
+            task: dataNewTask,
+            state: false,
+          });
+        } else {
+          dataTasks = [];
+          dataTasks.push({ id: 1, task: dataNewTask, state: false });
         }
+        miStorage.setItem("dataTasks", JSON.stringify(dataTasks));
+        setTaskPending(taskPending + 1);
+      } else {
+        alert("la tarea debe tener al menos tres caracteres");
+      }
+    } else {
+      alert("escribe alguna tarea");
     }
+  };
+  return [
+    {
+      arrayTask,
+      taskPending,
+    },
+    {
+      handleClickClear,
+      handleClickDelete,
+      handleClickRadio,
+      handleClickUpdate,
+      handleClickAdd,
+    },
+  ];
+};
 
-    const handleClickDelete=(id)=>{
-        var dataTasks = JSON.parse(miStorage.getItem("dataTasks"))
-        let deleteTask=dataTasks.filter((task)=>task.id!=id)
-        miStorage.setItem("dataTasks",JSON.stringify(deleteTask))
-        setArrayTask(deleteTask);
-        setTaskPending(taskPending-1)
-    }
-
-    const handleClickRadio=(boolean,idTask)=>{
-        var dataTasks = JSON.parse(miStorage.getItem("dataTasks"))
-        let updateTask=dataTasks.map((task)=>{
-            if(task.id==idTask){
-                task.state=boolean
-            }
-            return task;
-        })
-        miStorage.setItem("dataTasks",JSON.stringify(updateTask))
-        if(boolean) setTaskPending(taskPending-1)
-        else setTaskPending(taskPending+1)
-    }
-
-    const handleClickUpdate=(dataUpdateTask)=>{
-        miStorage.setItem("dataTasks",JSON.stringify(dataUpdateTask))
-        setTaskPending(null)
-    }
-
-    const handleClickAdd=(dataNewTask)=>{
-        if(dataNewTask.length>0){
-            var dataTasks = dataTasks=JSON.parse(miStorage.getItem("dataTasks"))
-            if(dataTasks){
-                dataTasks.push({"id":Math.max(...dataTasks.map(task => task.id))+1,"name":dataNewTask, "state":false})
-            }else{
-                dataTasks=[]
-                dataTasks.push({"id":1,"name":dataNewTask, "state":false})
-            }
-            miStorage.setItem("dataTasks",JSON.stringify(dataTasks))
-            setTaskPending(taskPending+1)
-        }else{
-            alert("escribe alguna tarea")
-        }
-    }
-    return [
-        {
-            arrayTask,
-            taskPending
-        },{
-            handleClickClear,
-            handleClickDelete,
-            handleClickRadio,
-            handleClickUpdate,
-            handleClickAdd
-        }
-    ]
-}
-
-export default useTask
+export default useTask;
